69bb42eec50fd0967add2692ccfd420b455911ff
Foaler <toomhomeprivateother@gmail.com>
2020-02-06 14:55:51 +0100

    Fixed

--- src/main/java/cc/foaler/api/API.java
+++ src/main/java/cc/foaler/api/API.java
@@ -1,20 +1,8 @@
 package cc.foaler.api;
 
-import cc.foaler.api.command.Command;
 import cc.foaler.api.command.CommandManager;
-import cc.foaler.api.event.Event;
 import cc.foaler.api.event.EventManager;
-import cc.foaler.api.module.Module;
 import cc.foaler.api.module.ModuleManager;
-import cc.foaler.api.utils.ClassScanner;
-import cc.foaler.api.utils.Client;
-import cc.foaler.api.utils.Init;
-import cc.foaler.api.utils.LogHelper;
-import one.util.streamex.StreamEx;
-
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.Set;
 
 public class API {
 
@@ -34,47 +22,6 @@ public class API {
 
     public void unregister() {
         eventManager.unregister(this);
-    }
-
-    public void initializeScanner(Class<?> mainClass, Client client, boolean loadModules, boolean loadCommands) {
-        StreamEx.of(ClassScanner.scanFile(null, mainClass)).filter(c -> {
-            try {
-                Class clazz = Class.forName(c);
-
-                return clazz.isAnnotationPresent(Init.class);
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            return false;
-        }).sorted(Comparator.comparingInt(c -> {
-            try {
-                Class clazz = Class.forName(c);
-
-                Init annotation = (Init) clazz.getAnnotation(Init.class);
-
-                return annotation.priority().getPriority();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            return 3;
-        })).forEachOrdered(c -> {
-            try {
-                Class clazz = Class.forName(c);
-
-                if(clazz.isAnnotationPresent(Init.class)) {
-                    Object obj = clazz.equals(mainClass) ? client : clazz.newInstance();
-                    Init annotation = (Init) clazz.getAnnotation(Init.class);
-
-                    if (loadModules && obj instanceof Module) {
-                        getModuleManager().addModule((Module) obj);
-                    } else if(loadCommands && obj instanceof Command) {
-                        getCommandManager().addCommand((Command) obj);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
     }
 
     public static void setInstance(API instance) {
